34f255eceec5c9f389836cb67a8e49b2573842ba
Bati <94386708+BatiBati@users.noreply.github.com>
2025-07-26 18:14:29 +0800

    Update

--- food-delivery-web-app/src/app/(user)/page.tsx
+++ food-delivery-web-app/src/app/(user)/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
     <div className="flex flex-col items-center">
       <TodayOfferFood />
       <CategoryNames />
+      
       <HomePageFoodMenu />
     </div>
   );
