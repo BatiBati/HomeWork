6ff0c2ea05999454e2ff93277e0980255e00f83b
24LP0893 24LP0893 <24LP0893@G304-CC0224.local>
2025-09-16 12:38:24 +0800

    ss

--- back/src/database/connect-todb.ts
+++ back/src/database/connect-todb.ts
@@ -4,7 +4,7 @@ import { connect } from "mongoose";
 
 export const connectToDataBase = async () => {
   try {
-    await connect(process.env.MONGO_URL as string);
+    await connect("mongodb+srv://codecatalysts2025_db_user:catalystsadmin123@cluster0.zqrxblc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("✅ MongoDB connected successfully");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
--- back/src/index.ts
+++ back/src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { connectToDataBase } from "../src/database/connect-todb";
 import { userRouter } from "../src/routers/user.route";
 import cors from "cors";
-
 import { config } from "dotenv";
 
 config();
@@ -14,18 +13,14 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors()
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Back end");
 });
-
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-
 app.use("/user", userRouter)
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port${port}`);
