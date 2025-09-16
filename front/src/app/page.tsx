46286c4d63c8f13fdf27c49b38742d3debf8a400
24LP0893 24LP0893 <24LP0893@G304-CC0224.local>
2025-09-16 12:23:03 +0800

    Db test

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+import { useEffect, useState } from "react";
+import { api } from "../../axios";
+
 export default function Home() {
+
+  const [data, setData] = useState("")
+  const handleSubmit = async () => {
+    await api.post("/user", { email: data })
+  }
+
+
   return (
     <div className="flex flex-col items-center relative">
-      Home Page
+      <input className="bg-red-500" onChange={(e) => setData(e.target.value)} />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
