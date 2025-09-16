ef6e27d397928b5a911ac024969c312d59304f99
24LP0893 24LP0893 <24LP0893@G304-CC0224.local>
2025-09-16 12:10:37 +0800

    s

--- back/src/routers/user.route.ts
+++ back/src/routers/user.route.ts
@@ -2,5 +2,5 @@ import { Router } from "express";
 import { createUserController } from "../controllers/user";
 
 
-export const userRouter = Router().post("/:id", createUserController)
+export const userRouter = Router().post("/", createUserController)
 
