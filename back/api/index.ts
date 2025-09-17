5e2116cdd70a0e20ddab93093335da61a794bbf9
24LP0893 24LP0893 <24LP0893@G304-CC0224.local>
2025-09-17 12:03:36 +0800

    Back degbug fixed

--- back/api/index.ts
+++ back/api/index.ts
@@ -3,6 +3,11 @@ import { connectToDataBase } from "../src/database/connect-todb";
 import { userRouter } from "../src/routers/user.route";
 import cors from "cors";
 import { config } from "dotenv";
+import { studentRouter } from "../src/routers/student.route";
+import { homeworkRouter } from "../src/routers/homework.route";
+import { teacherRouter } from "../src/routers/teacher.route";
+import { taskRouter } from "../src/routers/task.route";
+
 
 config();
 
@@ -13,15 +18,16 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Back end");
 });
-app.use("/user", userRouter)
-
+app.use("/user", userRouter);
+app.use("/student", studentRouter);
+app.use("/homework", homeworkRouter);
+app.use("/teacher", teacherRouter);
+app.use("/task", taskRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port${port}`);
