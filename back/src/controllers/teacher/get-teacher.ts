b81d8ebb17635cd6e74db15c206177b1d19a46c4
24LP2084 24LP2084 <24LP2084@G304-CC0235.local>
2025-09-17 13:01:13 +0800

    s

--- back/src/controllers/teacher/get-teacher.ts
+++ back/src/controllers/teacher/get-teacher.ts
@@ -3,7 +3,10 @@ import { teacherModel } from "../../models/teacher.model";
 
 export const getTeacherById: RequestHandler = async (req, res) => {
   try {
-    const teacher = await teacherModel.findById(req.params.id);
+    const teacher = await teacherModel
+      .findById(req.params.id)
+      .populate("students")
+      .populate("tasks"); // <--- энд populate хийж байна
     if (!teacher) {
       res.status(404).json({
         message: "Teacher not found",
