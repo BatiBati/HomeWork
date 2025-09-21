9180eb6ff6c0b4ddc14aa5751dd6884176ed7a61
24LP0893 24LP0893 <24LP0893@G304-CC0224.local>
2025-09-21 17:06:30 +0800

    connected

--- back/src/controllers/user/create-user.ts
+++ back/src/controllers/user/create-user.ts
@@ -31,6 +31,7 @@ export const createUserController: RequestHandler = async (req, res) => {
       role,
     });
 
+    
     res.status(200).json({ message: `User created`, user });
   } catch (error) {
     res.status(500).json({ error, message: "Create user server error" });
