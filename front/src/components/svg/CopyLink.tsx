8250367916f670f9f506814e18737c0471166399
BatiBati <bati@example.com>
2025-09-24 12:33:05 +0800

    copy link svg

---
+++ front/src/components/svg/CopyLink.tsx
@@ -0,0 +1,18 @@
+export const CopyLink = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#5da19e"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+      <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+    </svg>
+  );
+};
