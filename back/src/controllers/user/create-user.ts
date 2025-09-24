41306537f4ec688646dc34c35cb8360816f1a6fa
Bulgan Tamir <bulgantamir@Bulgans-MacBook-Air.local>
2025-09-24 14:30:22 +0800

    suragch nemeh bug fixed:

--- back/src/controllers/user/create-user.ts
+++ back/src/controllers/user/create-user.ts
@@ -3,7 +3,16 @@ import { userModel } from "../../models/user.model";
 import bcrypt from "bcrypt";
 
 export const createUserController: RequestHandler = async (req, res) => {
-  const { email, password, firstName, lastName, phoneNumber, role } = req.body;
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    phoneNumber,
+    role,
+    grade,
+    school,
+  } = req.body;
 
   if (!email || !password || !firstName || !lastName || !phoneNumber || !role) {
     res.status(400).json({ message: "All fields are required" });
@@ -29,9 +38,10 @@ export const createUserController: RequestHandler = async (req, res) => {
       lastName: lastName.trim().toLowerCase(),
       phoneNumber: phoneNumber,
       role,
+      grade,
+      school,
     });
 
-    
     res.status(200).json({ message: `User created`, user });
   } catch (error) {
     res.status(500).json({ error, message: "Create user server error" });
